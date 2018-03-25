73573d1ceaf191e3e95152adcd173e454750ddbd
Aryeh Raber <aryeh.raber@gmail.com>
2018-03-25 12:12:48 +0200

    Fix icon source URL for FA 4.7.0

--- FontAwesome/FontAwesomeController.php
+++ FontAwesome/FontAwesomeController.php
@@ -9,12 +9,12 @@ class FontAwesomeController extends Controller
 {
     public function getIcons()
     {
-        if ($cache = $this->cache->get('icons')) return $cache;
+        if (! $icons = $this->cache->get('icons')) {
+            $source = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/v4.7.0/src/icons.yml';
+            $icons = collect(YAML::parse(file_get_contents($source)))->get('icons');
 
-        $source = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/src/icons.yml';
-        $icons = collect(YAML::parse(file_get_contents($source))['icons']);
-
-        $this->cache->put('icons', $icons);
+            $this->cache->put('icons', $icons);
+        }
 
         return $icons;
     }
